44fbf4b649960e15b297350801a4b034a1cc7b60
kab0718 <kab3nan9@gmail.com>
2020-02-12 01:40:17 +0900

    メールアドレスなどがnullを許容しないようにした

--- db/migrate/20200210082352_create_tasks.rb
+++ db/migrate/20200210082352_create_tasks.rb
@@ -1,10 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :name, limit: 30
+      t.string :name, limit: 30, null: false
       t.text :description
 
-      t.timestamps null: false
+      t.timestamps
       t.index :name, unique: true
     end
   end
